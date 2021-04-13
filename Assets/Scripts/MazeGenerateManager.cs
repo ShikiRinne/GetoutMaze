6d57de4d4425cc497c8e4f00ed39e90741d0aec3
ShikiRinne <78026848+ShikiRinne@users.noreply.github.com>
2021-04-14 00:10:57 +0900

    Preparing for Enemy Generation
    Get All DeadendPoint to Place Enemy at farthest DeadendPoint.

--- Assets/Scripts/MazeGenerateManager.cs
+++ Assets/Scripts/MazeGenerateManager.cs
@@ -28,20 +28,21 @@ public class MazeGenerateManager: MonoBehaviour
 
     private GameObject PlayerClone;
 
-    private int MemoCount;
-
     private List<int> PlacementObjectList;
     private List<int> DeadendPointList;
     private List<GameObject> MemoStorageList = new List<GameObject>();
 
     public Vector3 PassRestartPos { get; private set; }
+    public float StartDirection { get; private set; }
+    public int PassTotalSplitMemos { get; private set; } = 4;
 
-    private enum ObjectType
+    private enum MazePoint
     {
         Path = 0,
         Wall = 1,
         Start = 2,
-        Exit = 3
+        Exit = 3,
+        DeadPoint = 4
     }
 
     private enum DirectionType
@@ -51,10 +52,6 @@ public class MazeGenerateManager: MonoBehaviour
         LEFT = 2,
         UP = 3
     }
-
-    public float StartDirection { get; private set; }
-
-    public int PassTotalSplitMemos { get; private set; } = 4;
 
     //配置した後メモを管理するKeyCodeMemoで使用するためプロパティで受け渡しできるようにする
     public List<int> NotePositionList { get; set; }
@@ -100,11 +97,11 @@ public class MazeGenerateManager: MonoBehaviour
                 if (y == 0 || y == MazeHight - 1 ||
                     x == 0 || x == MazeWidth - 1)
                 {
-                    Maze[x, y] = (int)ObjectType.Wall;
+                    Maze[x, y] = (int)MazePoint.Wall;
                 }
                 else
                 {
-                    Maze[x, y] = (int)ObjectType.Path;
+                    Maze[x, y] = (int)MazePoint.Path;
                 }
             }
         }
@@ -118,7 +115,7 @@ public class MazeGenerateManager: MonoBehaviour
             for (int x = 2; x < MazeWidth - 1; x += 2)
             {
                 //棒倒し法の基準となる壁の生成
-                Maze[x, y] = (int)ObjectType.Wall;
+                Maze[x, y] = (int)MazePoint.Wall;
 
                 //棒倒し法でマップ内が迷路状になるよう壁を生成
                 while (true)
@@ -152,9 +149,9 @@ public class MazeGenerateManager: MonoBehaviour
                             break;
                     }
                     //選出された方向に壁がなければ生成して抜ける、あれば繰り返す
-                    if (Maze[AddXWall, AddYWall] != (int)ObjectType.Wall)
+                    if (Maze[AddXWall, AddYWall] != (int)MazePoint.Wall)
                     {
-                        Maze[AddXWall, AddYWall] = (int)ObjectType.Wall;
+                        Maze[AddXWall, AddYWall] = (int)MazePoint.Wall;
                         break;
                     }
                 }
@@ -171,21 +168,21 @@ public class MazeGenerateManager: MonoBehaviour
             for (int x = 0; x < MazeWidth; ++x)
             {
                 //四方のどこに壁があるかチェック、いくつの方向に壁があるかカウント
-                if (Maze[x, y] == (int)ObjectType.Path)
+                if (Maze[x, y] == (int)MazePoint.Path)
                 {
-                    if (Maze[x + 1, y] == (int)ObjectType.Wall)
+                    if (Maze[x + 1, y] == (int)MazePoint.Wall)
                     {
                         WallCount++;
                     }
-                    if (Maze[x - 1, y] == (int)ObjectType.Wall)
+                    if (Maze[x - 1, y] == (int)MazePoint.Wall)
                     {
                         WallCount++;
                     }
-                    if (Maze[x, y + 1] == (int)ObjectType.Wall)
+                    if (Maze[x, y + 1] == (int)MazePoint.Wall)
                     {
                         WallCount++;
                     }
-                    if (Maze[x, y - 1] == (int)ObjectType.Wall)
+                    if (Maze[x, y - 1] == (int)MazePoint.Wall)
                     {
                         WallCount++;
                     }
@@ -194,6 +191,7 @@ public class MazeGenerateManager: MonoBehaviour
                 //三方向に壁がある位置を行き止まりのリストに追加
                 if (WallCount >= 3)
                 {
+                    Maze[x, y] = (int)MazePoint.DeadPoint;
                     DeadendPointList.Add(MapNumber);
                     WallCount = 0;
                 }
@@ -217,21 +215,20 @@ public class MazeGenerateManager: MonoBehaviour
         }
 
         //行き止まりの位置からランダムに選出
-        MemoCount = PassTotalSplitMemos;
         NotePositionList = new List<int>();
         int RandomPoint;
-        for (int i = 0; i < MemoCount + 2; ++i)
+        for (int i = 0; i < PassTotalSplitMemos + 2; ++i)
         {
             RandomPoint = Random.Range(0, DeadendPointList.Count);
             
             //先にスタートとゴールの位置を選出し、残りをメモを置く位置とする
             if (i == 0)
             {
-                PlacementObjectList[DeadendPointList[RandomPoint]] = (int)ObjectType.Start;
+                PlacementObjectList[DeadendPointList[RandomPoint]] = (int)MazePoint.Start;
             }
             else if (i == 1)
             {
-                PlacementObjectList[DeadendPointList[RandomPoint]] = (int)ObjectType.Exit;
+                PlacementObjectList[DeadendPointList[RandomPoint]] = (int)MazePoint.Exit;
             }
             else
             {
@@ -262,33 +259,33 @@ public class MazeGenerateManager: MonoBehaviour
                 
                 switch (PlacementObjectList[Count])
                 {
-                    case (int)ObjectType.Path:
+                    case (int)MazePoint.Path:
                         //メモを置く位置であればメモを置く
                         if (NotePositionList.Contains(Count))
                         {
                             MemoStorageList.Add(Instantiate(Memo, new Vector3(x, 0, y), Quaternion.identity));
                         }                        
                         break;
-                    case (int)ObjectType.Wall:
+                    case (int)MazePoint.Wall:
                         //壁を配置
                         Instantiate(Wall, new Vector3(x, 0, y), Quaternion.identity);
                         break;
-                    case (int)ObjectType.Start:
+                    case (int)MazePoint.Start:
                         //プレイヤーを配置
                         //プレイヤーが出現する向きを通路側に向ける
-                        if (Maze[x + 1, y] == (int)ObjectType.Path)
+                        if (Maze[x + 1, y] == (int)MazePoint.Path)
                         {
                             StartDirection = 90f;
                         }
-                        if (Maze[x, y - 1] == (int)ObjectType.Path)
+                        if (Maze[x, y - 1] == (int)MazePoint.Path)
                         {
                             StartDirection = 180f;
                         }
-                        if (Maze[x, y + 1] == (int)ObjectType.Path)
+                        if (Maze[x, y + 1] == (int)MazePoint.Path)
                         {
                             StartDirection = 0f;
                         }
-                        if (Maze[x - 1, y] == (int)ObjectType.Path)
+                        if (Maze[x - 1, y] == (int)MazePoint.Path)
                         {
                             StartDirection = -90f;
                         }
@@ -296,7 +293,7 @@ public class MazeGenerateManager: MonoBehaviour
                         PassRestartPos = new Vector3(x, StartPoint.transform.localScale.y, y);
                         PlayerClone = Instantiate(StartPoint, PassRestartPos, Quaternion.identity);
                         break;
-                    case (int)ObjectType.Exit:
+                    case (int)MazePoint.Exit:
                         //出口を配置
                         Instantiate(ExitPoint, new Vector3(x, 0, y), Quaternion.identity);
                         break;
@@ -308,11 +305,32 @@ public class MazeGenerateManager: MonoBehaviour
     }
 
     /// <summary>
+    /// エネミーの生成
+    /// </summary>
+    public void CreateEnemy()
+    {
+        for (int y = 0; y < MazeHight; ++y)
+        {
+            for (int x = 0; x < MazeWidth; ++x)
+            {
+                if (Maze[x, y] == (int)MazePoint.DeadPoint)
+                {
+                    //突き当りの位置とプレイヤーの位置の距離の絶対値を出す
+                    //一番遠い位置を保存して最終的に残った位置にエネミーを生成
+                }
+            }
+        }
+    }
+
+    /// <summary>
     /// プレイヤーの状態のリセット
     /// </summary>
     public void CharaPosReset()
     {
+        //プレイヤーとの親子関係（MainCamera）を解除
         PlayerClone.transform.DetachChildren();
+
+        //ステージ上のプレイヤーを一旦削除し再生成
         Destroy(PlayerClone);
         PlayerClone = Instantiate(StartPoint, PassRestartPos, Quaternion.identity);
     }
