9ed7b0b816f15864bbb9ca82a5f2e21bf3751f74
ShikiRinne <78026848+ShikiRinne@users.noreply.github.com>
2021-04-29 17:14:23 +0900

    Import NavMeshCompornents
    For Baking of Dynamic NavMesh.

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -1,12 +1,22 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class Enemy : MonoBehaviour
 {
+    private NavMeshAgent Agent;
+
+    public enum EnemyState
+    {
+        Wandering,
+        Chase,
+        Illuminated
+    }
+
     void Start()
     {
-        
+        Agent = gameObject.GetComponent<NavMeshAgent>();
     }
 
     void Update()
