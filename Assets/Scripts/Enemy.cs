d8f0016463a517b153cb42c4993334b6bbbe37ec
ShikiRinne <78026848+ShikiRinne@users.noreply.github.com>
2021-05-05 01:28:07 +0900

    no title

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -16,6 +16,10 @@ public class Enemy : MonoBehaviour
     private float SearchLength = 0f;
     [SerializeField]
     private float ChaseLength = 0f;
+    [SerializeField]
+    private float WaitTime = 0f;
+
+    private bool IsSearchNext = false;
 
     public enum EnemyState
     {
@@ -23,6 +27,7 @@ public class Enemy : MonoBehaviour
         Chase,
         Illuminated
     }
+    public EnemyState NowState;
 
     void Start()
     {
@@ -30,22 +35,55 @@ public class Enemy : MonoBehaviour
 
         Agent = gameObject.GetComponent<NavMeshAgent>();
         transform.Rotate(0f, MGM.EnemyStartDir, 0f);
+
+        NextTarget();
+        NowState = EnemyState.Wandering;
     }
 
     void Update()
     {
-        NextTarget();
         Agent.isStopped = GameManager.GameManager_Instance.IsEnemyStop;
+
+        EnemyStateMove(NowState);
+    }
+
+    private void EnemyStateMove(EnemyState state)
+    {
+        switch (state)
+        {
+            case EnemyState.Wandering:
+                if (Agent.remainingDistance == 0f)
+                {
+                    Target = null;
+                    NextTarget();
+                }
+                break;
+            case EnemyState.Chase:
+                break;
+            case EnemyState.Illuminated:
+                break;
+        }
+    }
+
+    private IEnumerator ArriveTarget()
+    {
+        Agent.isStopped = true;
+        yield return new WaitForSeconds(WaitTime);
+
+        Agent.isStopped = false;
+        yield return null;
     }
 
     private void NextTarget()
     {
-        if (Target == null ||
-            (gameObject.transform.position.x == Target.transform.position.x &&
-             gameObject.transform.position.z == Target.transform.position.z))
+        StartCoroutine(ArriveTarget());
+
+        if (Target == null)
         {
-            Target = MGM.DeadendObjectList[Random.Range(0, MGM.DeadendObjectList.Count)];
+            int random = Random.Range(0, MGM.DeadendObjectList.Count);
+            Target = MGM.DeadendObjectList[random];
             Agent.SetDestination(Target.transform.position);
         }
+        Debug.Log("nextpos:(" + Target.transform.position.x + ", " + Target.transform.position.z + ")");
     }
 }
