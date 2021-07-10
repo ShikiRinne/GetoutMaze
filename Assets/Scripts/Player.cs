5d99cf6b33055740654d912b3cc1a3106dd83cd5
ShikiRinne <78026848+ShikiRinne@users.noreply.github.com>
2021-07-11 01:26:44 +0900

    Fix Hand
    Change Hand to Box instead of ray.

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -15,6 +15,8 @@ public class Player : MonoBehaviour
     private GameObject MainCamera;
     [SerializeField]
     private GameObject Psyllium = default;
+    [SerializeField]
+    private GameObject HandArea = default;
 
     [SerializeField]
     private float SetMoveSpeed = 0f;
