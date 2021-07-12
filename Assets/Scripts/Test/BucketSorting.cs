1ff7224ab0f92471fb8fe0ef72c04a50b84deae1
ShikiRinne <78026848+ShikiRinne@users.noreply.github.com>
2021-07-13 00:55:59 +0900

    Create Script to test sort
    To randomize Memo Pickup.

---
+++ Assets/Scripts/Test/BucketSorting.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BucketSorting : MonoBehaviour
+{
+    private List<int> NumberList = new List<int>(); //元のリスト
+    private List<int> PickList = new List<int>();   //ランダムにピックした数値を入れるリスト
+    private List<int> SortList = new List<int>();   //ソートするためのリスト
+
+    private int Pick = 0;
+
+    void Start()
+    {
+        for (int i = 0; i < 10; ++i)
+        {
+            NumberList.Add(Random.Range(0, 10));
+            Debug.Log("NumberList[" + i + "] = " + NumberList[i]);
+        }
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Return))
+        {
+            BucketSort(NumberList);
+        }
+    }
+
+    private void BucketSort(List<int> items)
+    {
+        foreach (int item in items)
+        {
+            SortList.Add(item);
+        }
+
+        int index = 0;
+        for (int i = 0; i < SortList.Count; ++i)
+        {
+            for (int j = 0; j < SortList[i]; ++j)
+            {
+                SortList[index] = i;
+                Debug.Log("Sort[" + i + "] = " + SortList[index]);
+
+                index++;
+            }
+        }
+    }
+}
