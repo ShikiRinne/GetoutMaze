878866a0e8eadc6daee4afff6b512a562be84bff
ShikiRinne <78026848+ShikiRinne@users.noreply.github.com>
2021-07-28 02:06:36 +0900

    Processing Change
    Added Camera rotation to Player processing.
    HandArea will be changed BoxCast instead of Collider.

--- Assets/Scripts/Test/hand_test.cs
+++ Assets/Scripts/Test/hand_test.cs
@@ -4,6 +4,11 @@ using UnityEngine;
 
 public class hand_test : MonoBehaviour
 {
+    private List<GameObject> touchitemlist = new List<GameObject>();
+
+    private GameObject notesobj;
+    private bool istouchnotes = false;
+
     void Start()
     {
         
@@ -11,11 +16,49 @@ public class hand_test : MonoBehaviour
 
     void Update()
     {
-        
+        TouchNotes(notesobj);
     }
 
     private void OnTriggerStay(Collider other)
     {
         Debug.Log(other.gameObject.tag);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        //if (other.gameObject.CompareTag("Notes"))
+        //{
+        //    istouchnotes = true;
+        //}
+        touchitemlist.Add(other.gameObject);
+        notesobj = other.gameObject;
+
+        Debug.Log(other.name);
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        //if (other.gameObject.CompareTag("Notes"))
+        //{
+        //    istouchnotes = false;
+        //}
+        touchitemlist.Remove(other.gameObject);
+
+        Debug.Log(other.name);
+    }
+
+    private void TouchNotes(GameObject touch)
+    {
+        if (Input.GetMouseButtonDown(0)/* && istouchnotes*/)
+        {
+            foreach (GameObject notes in touchitemlist)
+            {
+                if (notes.CompareTag("Notes"))
+                {
+                    touchitemlist.Remove(notes);
+                    Destroy(touch);
+                }
+            }
+        }
     }
 }
--- Assets/Scripts/Test/move_test.cs
+++ Assets/Scripts/Test/move_test.cs
@@ -5,19 +5,29 @@ using UnityEngine;
 public class move_test : MonoBehaviour
 {
     private CharacterController chara;
+    private GameObject MainCamera;
 
     private Vector3 Horizontal;
     private Vector3 Vertical;
     private Vector3 Direction;
 
+    private float CameraRotate;
+
     void Start()
     {
         chara = GetComponent<CharacterController>();
+        MainCamera = GameObject.Find("Main Camera");
+        MainCamera.transform.parent = gameObject.transform;
+        MainCamera.transform.localPosition = new Vector3(MainCamera.transform.localPosition.x, MainCamera.transform.localPosition.y, 0.1f);
+
+        Cursor.visible = false;
+        Cursor.lockState = CursorLockMode.Locked;
     }
 
     void Update()
     {
         PlayerMove();
+        PlayerRotate();
     }
 
     private void PlayerMove()
@@ -27,5 +37,13 @@ public class move_test : MonoBehaviour
         Direction = Horizontal + Vertical;
 
         chara.Move(Time.deltaTime * Direction.normalized);
+    }
+
+    private void PlayerRotate()
+    {
+        CameraRotate -= Input.GetAxisRaw("Mouse Y");
+
+        transform.Rotate(0, Input.GetAxisRaw("Mouse X"), 0);
+        MainCamera.transform.localEulerAngles = new Vector3(CameraRotate, 0, 0);
     }
 }
